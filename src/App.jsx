3933d1d5d2cf75efacd8ee9e20ae7691b713256b
Sudin Shrestha <165158966+sshresth08@users.noreply.github.com>
2025-09-10 23:39:00 -0700

    100.Handling events and Using Two-Way-Binding

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import Header from "./components/Header";
+import UserInput from "./components/UserInput";
+/* Siblings Jsx Element are not allowed as the main value being returned or stored in variable or used as a value in general
+    we must have only one Root Jsx element which may contain many Siblings Elments as needed
+*/
 function App() {
   return (
-    <h1>React Investment Calculator</h1>
-  )
+    <ol>
+      <Header />
+      <UserInput />
+    </ol>
+  );
 }
 
-export default App
+export default App;
