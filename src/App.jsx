7b9d59d032f87396f40e893200e27f914f02585f
Sudin Shrestha <165158966+sshresth08@users.noreply.github.com>
2025-09-11 00:03:47 -0700

    101:Lifting State Up

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
+import { useState } from "react"; 
 import Header from "./components/Header";
 import UserInput from "./components/UserInput";
+import Results from "./components/Results";
 /* Siblings Jsx Element are not allowed as the main value being returned or stored in variable or used as a value in general
     we must have only one Root Jsx element which may contain many Siblings Elments as needed
 */
 function App() {
+  const[userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 7,
+    duration: 10,
+  });
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: newValue,
+      };
+    });
+  }
+
   return (
     <ol>
       <Header />
-      <UserInput />
+      <UserInput userInput={userInput} onChange={handleChange}/>
+      <Results userInput={userInput}/>
     </ol>
   );
 }
--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,22 +1,7 @@
 import { useState } from "react";
 
-export default function UserInput() {
-  const[userInput, setUserInput] = useState({
-    initialInvestment: 10000,
-    annualInvestment: 1200,
-    expectedReturn: 7,
-    duration: 10,
-  });
-
-  function handleChange(inputIdentifier, newValue) {
-    setUserInput((prevUserInput) => {
-      return {
-        ...prevUserInput,
-        [inputIdentifier]: newValue,
-      };
-    });
-  }
-
+export default function UserInput({ onChange, userInput }) {
+  
   return (
     <section id="user-input">
       <div className="input-group">
@@ -27,7 +12,7 @@ export default function UserInput() {
             required
             value={userInput.initialInvestment}
             onChange={(event) =>
-              handleChange("initialInvestment", event.target.value)
+              onChange("initialInvestment", event.target.value)
             }
           />
         </p>
@@ -37,8 +22,8 @@ export default function UserInput() {
             type="number"
             required
             value={userInput.annualInvestment}
-            onChnage={(event) =>
-              handleChange("annualInvestment", event.target.value)
+            onChange={(event) =>
+              onChange("annualInvestment", event.target.value)
             }
           />
         </p>
@@ -51,7 +36,7 @@ export default function UserInput() {
             required
             value={userInput.expectedReturn}
             onChange={(event) =>
-              handleChange("expectedReturn", event.target.value)
+              onChange("expectedReturn", event.target.value)
             }
           />
         </p>
@@ -61,7 +46,7 @@ export default function UserInput() {
             type="number"
             required
             value={userInput.duration}
-            onChange={(event) => handleChange("duration", event.target.value)}
+            onChange={(event) => onchange("duration", event.target.value)}
           />
         </p>
       </div>
